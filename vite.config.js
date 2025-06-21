ab7408faf389ab804ea144ad2d0c5940f01b32e9
bosummz1884 <Brandonjulian16@gmail.com>
2025-06-21 16:06:38 -0500

    added @lib alias

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,8 @@ export default defineConfig({
       '@utils': path.resolve(__dirname, 'src/utils'),
       '@attached_assets': path.resolve(__dirname, 'src/attached_assets'),
       '@ui': path.resolve(__dirname, 'components/ui'),
-      "@hooks": path.resolve(__dirname, "src/hooks"),
+      '@hooks': path.resolve(__dirname, "src/hooks"),
+      '@lib': path.resolve(__dirname, "src/lib"),
     }
   },
 
